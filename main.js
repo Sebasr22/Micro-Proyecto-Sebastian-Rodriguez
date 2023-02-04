83019ab873771b2b3a2a3985271de400d2f03123
Sebasr22 <r.sebastian2020@correo.unimet.edu.ve>
2023-02-04 15:58:42 -0400

    Experiencia. Revisar el height: auto

--- main.js
+++ main.js
@@ -12,3 +12,31 @@ function showSlides() {
     slides[slideIndex - 1].style.opacity = 1;
     setTimeout(showSlides, 5000);
 }
+
+// Carrusel experiencia
+const boxes = document.querySelectorAll('.box');
+const leftArrow = document.querySelector('.left');
+const rightArrow = document.querySelector('.right');
+
+leftArrow.addEventListener('click', () => {
+    moveLeft();
+});
+
+rightArrow.addEventListener('click', () => {
+    moveRight();
+});
+
+function moveLeft() {
+    const activeBox = document.querySelector('.active');
+    const nextBox = activeBox.previousElementSibling || boxes[boxes.length - 1];
+
+    activeBox.classList.remove('active');
+    nextBox.classList.add('active');
+}
+
+function moveRight() {
+    const activeBox = document.querySelector('.active');
+    const nextBox = activeBox.nextElementSibling || boxes[0];
+    activeBox.classList.remove('active');
+    nextBox.classList.add('active');
+}
